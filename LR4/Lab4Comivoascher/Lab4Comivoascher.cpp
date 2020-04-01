2fd04d8b2233fdf13f50deb20b307cffb79fe853
NazarKanonik <48983886+NazarKanonik@users.noreply.github.com>
2020-04-01 14:59:29 +0300

    Add files via upload

---
+++ LR4/Lab4Comivoascher/Lab4Comivoascher.cpp
@@ -0,0 +1,44 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int inf = 1E9, NMAX = 20;// Кількість вершин(міст)
+int n, i, j, k, m, temp, ans, d[NMAX][NMAX], t[1 << NMAX][NMAX];
+
+bool get(int nmb, int x)
+{
+	return (x&(1 << nmb)) != 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "1)Введiть кiлькiсть вершин V ." << '\n' << "2)Введiть матницю VxV iз довжинами ребер мiж вершинами." << '\n';
+	//Щитуємо введені дані
+	cin >> n;
+	for (i = 0; i<n; ++i)
+		for (j = 0; j<n; ++j) cin >> d[i][j];
+	t[1][0] = 0; m = 1 << n;
+	for (i = 1; i<m; i += 2)
+		for (j = (i == 1) ? 1 : 0; j<n; ++j)
+		{
+			t[i][j] = inf;
+			if (j>0 && get(j, i))
+			{
+				temp = i ^ (1 << j);
+				for (k = 0; k<n; ++k)
+					if (get(k, i) && d[k][j]>0) t[i][j] = min(t[i][j], t[temp][k] + d[k][j]);//вибираємо менше ребро
+			}
+		}
+	cout << "Результат: довжина оптимального маршруту комiвояжера   ";
+	for (j = 1, ans = inf; j<n; ++j)
+		if (d[j][0]>0) ans = min(ans, t[m - 1][j] + d[j][0]); 
+	if (ans == inf) cout << "Не можна порахувати("; else cout << ans;
+
+	system("pause");
+
+}
